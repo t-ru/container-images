8f670d2712a5c7f8fe31f35eb32e787155414c94
Thomas Rudolph <thomas.rudolph@gmail.com>
2021-06-25 12:37:27 +0200

    RMT  initialization added

--- rmt-server/container-entrypoint.d/21-rmt-server.sh
+++ rmt-server/container-entrypoint.d/21-rmt-server.sh
@@ -1,3 +1,35 @@
 #!/bin/sh
+set -e
 
-echo "hello from 20-rmt-server.sh"
+# PV could be empty, make sure the directories exist
+mkdir -p /var/lib/rmt/public/repo
+mkdir -p /var/lib/rmt/public/suma
+mkdir -p /var/lib/rmt/regsharing
+mkdir -p /var/lib/rmt/tmp
+# Set permissions
+chown -R _rmt:nginx /var/lib/rmt
+
+if [ -z "${MYSQL_HOST}" ]; then
+	echo "MYSQL_HOST not set!"
+	exit 1
+fi
+if [ -z "${MYSQL_PASSWORD}" ]; then
+        echo "MYSQL_PASSWORD not set!"
+        exit 1
+fi
+
+MYSQL_DATABASE="${MYSQL_DATABASE:-rmt}"
+MYSQL_USER="${MYSQL_USER:-rmt}"
+
+# Create adjusted /etc/rmt.conf
+echo -e "database:\n  host: ${MYSQL_HOST}\n  database: ${MYSQL_DATABASE}\n  username: ${MYSQL_USER}\n  password: ${MYSQL_PASSWORD}" > /etc/rmt.conf
+echo -e "  adapter: mysql2\n  encoding: utf8\n  timeout: 5000\n  pool: 5\n" >> /etc/rmt.conf
+echo -e "scc:\n  username: ${SCC_USERNAME}\n  password:  ${SCC_PASSWORD}\n  sync_systems: true\n" >> /etc/rmt.conf
+echo -e "log_level:\n  rails: debug" >> /etc/rmt.conf
+
+
+# Create / migrate RMT database
+echo "Create / migrate RMT database"
+pushd /usr/share/rmt > /dev/null
+/usr/share/rmt/bin/rails db:create db:migrate RAILS_ENV=production
+popd > /dev/null
