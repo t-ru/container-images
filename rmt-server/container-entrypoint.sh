ae808af6b9dd9af3f4caebfbedbcd136a1dc2282
Thomas Rudolph <thomas.rudolph@gmail.com>
2021-07-26 22:39:10 +0200

    code enabled

--- rmt-server/container-entrypoint.sh
+++ rmt-server/container-entrypoint.sh
@@ -6,52 +6,52 @@ set -e
 trap "{ exit 0; }" TERM INT
 
 
-#if [ -d "/container-entrypoint.d" ]; then
-#    chown root:root /container-entrypoint.d
-#    chmod 755 /container-entrypoint.d
-#    for _container_entrypoint in /container-entrypoint.d/*; do
-#        _extension="${_container_entrypoint##*.}"
-#        if [ -f "${_container_entrypoint}" ] && [ "${_extension}" = "env" ]; then
-#            chown root:root "${_container_entrypoint}"
-#            chmod 644 "${_container_entrypoint}"
-#        elif [ -f "${_container_entrypoint}" ] && [ "${_extension}" = "sh" ]; then
-#            chown root:root "${_container_entrypoint}"
-#            chmod 755 "${_container_entrypoint}"
-#        fi
-#    done
-#fi
+if [ -d "/container-entrypoint.d" ]; then
+    chown root:root /container-entrypoint.d
+    chmod 755 /container-entrypoint.d
+    for _container_entrypoint in /container-entrypoint.d/*; do
+        _extension="${_container_entrypoint##*.}"
+        if [ -f "${_container_entrypoint}" ] && [ "${_extension}" = "env" ]; then
+            chown root:root "${_container_entrypoint}"
+            chmod 644 "${_container_entrypoint}"
+        elif [ -f "${_container_entrypoint}" ] && [ "${_extension}" = "sh" ]; then
+            chown root:root "${_container_entrypoint}"
+            chmod 755 "${_container_entrypoint}"
+        fi
+    done
+fi
 
 
 
 # Process files in /container-entrypoint.d
-#if [ -d "/container-entrypoint.d" ]; then
-#    for _container_entrypoint in /container-entrypoint.d/*; do
-#        _extension="${_container_entrypoint##*.}"
-#        if [ -f "${_container_entrypoint}" ] && [ "${_extension}" = "env" ]; then
-#            
-#            # source env files
-#            #echo "Sourcing: ${_container_entrypoint} $@"
-#            #set -a && . "${_container_entrypoint}" "$@" && set +a
-#
-#            #echo ""
-#            #echo "---- Sourcing ${_container_entrypoint} ----"
-#            set -a && . "${_container_entrypoint}" && set +a
-#            #echo ""
-#            
-#        elif [ -f "${_container_entrypoint}" ] && [ "${_extension}" = "sh" ] && [ -x "${_container_entrypoint}" ]; then
-#            
-#            # run script files
-#            #echo "Executing: ${_container_entrypoint} $@"
-#            #"${_container_entrypoint}" "$@"
-#            
-#            #echo ""
-#            #echo "---- Executing ${_container_entrypoint} ----"
-#            "${_container_entrypoint}"
-#            #echo ""
-#
-#        fi
-#    done
-#fi
+if [ -d "/container-entrypoint.d" ]; then
+    for _container_entrypoint in /container-entrypoint.d/*; do
+        _extension="${_container_entrypoint##*.}"
+        if [ -f "${_container_entrypoint}" ] && [ "${_extension}" = "env" ]; then
+            
+            # source env files
+            #echo "Sourcing: ${_container_entrypoint} $@"
+            #set -a && . "${_container_entrypoint}" "$@" && set +a
+
+            #echo ""
+            #echo "---- Sourcing ${_container_entrypoint} ----"
+            set -a && . "${_container_entrypoint}" && set +a
+            #echo ""
+            
+        elif [ -f "${_container_entrypoint}" ] && [ "${_extension}" = "sh" ] && [ -x "${_container_entrypoint}" ]; then
+            
+            # run script files
+            #echo "Executing: ${_container_entrypoint} $@"
+            #"${_container_entrypoint}" "$@"
+            
+            #echo ""
+            #echo "---- Executing ${_container_entrypoint} ----"
+            "${_container_entrypoint}"
+            #echo ""
+
+        fi
+    done
+fi
 
 #for _file in /container-entrypoint.d/*; do
 #    _file_extension="${_file##*.}"
